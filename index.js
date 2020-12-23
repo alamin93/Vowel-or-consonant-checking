0f35c83699cf81d7c0c3cf7bbac11ed3841a4afa
S M AL AMIN <76505639+alamin93@users.noreply.github.com>
2020-12-23 11:04:47 +0600

    added html css file for vowel & consonant checking

---
+++ index.js
@@ -0,0 +1,23 @@
+//Vowel
+var letter=prompt("Enter A letter for checking vowel");
+var letter = letter.toLowerCase();
+switch(letter){
+    case "a":
+    console.log("Vowel");
+    break;
+    case "e":
+    console.log("Vowel");
+    break;
+    case "i":
+    console.log("Vowel");
+    break;
+    case "o":
+    console.log("Vowel");
+    break;
+    case "u":
+    console.log("Vowel");
+    break;
+    default:
+        console.log("Consonant");
+}
+
